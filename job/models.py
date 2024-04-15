a8319df9cd0780c06c71dfe6c67212bdf3038342
mahmoud-badri <eng.mahmoud.badrii@gmail.com>
2024-04-15 02:08:22 +0200

    finish job models

--- job/models.py
+++ job/models.py
@@ -1,5 +1,7 @@
+from typing import Iterable
 from django.db import models
-
+from django.utils.text import slugify
+from django.contrib.auth.models import User
 # Create your models here.
 
 JOB_TYPE = (
@@ -15,6 +17,7 @@ def image_upload(instance,filename):
 
 
 class Job (models.Model):
+    owner = models.ForeignKey(User, related_name='job_owner', on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     #location
     job_type = models.CharField(max_length= 20, choices=JOB_TYPE)
@@ -25,15 +28,34 @@ class Job (models.Model):
     experience = models.IntegerField(default=1)
     category = models.ForeignKey("Category", on_delete=models.CASCADE)
     image = models.ImageField(upload_to=image_upload)
-    
+    slug = models.SlugField(null=True, blank=True)
     
     
     def __str__(self):
         return self.title
     
+    
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title) 
+        return super().save(*args, **kwargs)
 
 class Category (models.Model):
     name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+    
+    
+    
+class Apply(models.Model):
+    job = models.ForeignKey(Job, on_delete=models.CASCADE, related_name='applay_job')
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    web_site = models.URLField(null=True)
+    cv = models.FileField(upload_to='apply/')
+    cover_letter = models.TextField(max_length=700)
+    created_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.name
--- job/urls.py
+++ job/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', job_list, name='job_list'),  # URL for the job list view
-    path('<int:id>/', job_detail, name='job_detail'),  # URL for the job detail view
+    path('add/', add_job, name='add_job'),  # URL for the job list view
+    path('<str:slug>/', job_detail, name='job_detail'),  # URL for the job detail view
 ]
